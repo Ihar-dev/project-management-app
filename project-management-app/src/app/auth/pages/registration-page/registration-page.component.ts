0e84ea7371076e0c5ff64dacdcfbb53b51455c2f
Tetiana Maslova <tetiana.maslova.qa@gmail.com>
2022-04-26 00:22:48 +0200

    feat: add registration form-group

--- project-management-app/src/app/auth/pages/registration-page/registration-page.component.ts
+++ project-management-app/src/app/auth/pages/registration-page/registration-page.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+const MIN_LENGTH_FIRST_NAME = 2;
+const MIN_LENGTH_LOGIN = 8;
 
 @Component({
   selector: 'app-registration-page',
@@ -7,7 +10,24 @@ import { FormBuilder } from '@angular/forms';
   styleUrls: ['./registration-page.component.scss'],
 })
 export class RegistrationPageComponent implements OnInit {
-  constructor(private fb: FormBuilder) {}
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      name: [
+        '',
+        [Validators.required, Validators.minLength(MIN_LENGTH_FIRST_NAME)],
+      ],
+      login: [
+        '',
+        [Validators.required, Validators.minLength(MIN_LENGTH_LOGIN)],
+      ],
+      passwords: this.fb.group({
+        password: ['', Validators.compose([Validators.required])],
+        confirmPassword: ['', [Validators.required]],
+      }),
+    });
+  }
 
   ngOnInit(): void {}
 }
