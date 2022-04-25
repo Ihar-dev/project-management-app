0df11fa096c755145408e85b8697bf7424e97eb3
Ihar Kozik <ihar.kozik@gmail.com>
2022-04-25 19:44:38 +0300

    refactor: change structure of the project

--- project-management-app/src/app/app.component.ts
+++ project-management-app/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'project-management-system';
+  title = 'project-management-app';
 }
