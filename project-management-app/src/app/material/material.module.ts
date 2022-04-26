c36d0aa75276ea678830522199e46a05042e3ea4
Tetiana Maslova <tetiana.maslova.qa@gmail.com>
2022-04-26 02:27:41 +0200

    feat: add registration form layout

--- project-management-app/src/app/material/material.module.ts
+++ project-management-app/src/app/material/material.module.ts
@@ -3,12 +3,14 @@ import { MatSliderModule } from '@angular/material/slider';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 const MaterialComponents = [
   MatSliderModule,
   MatInputModule,
   MatFormFieldModule,
   MatIconModule,
+  MatButtonModule,
 ];
 
 @NgModule({
