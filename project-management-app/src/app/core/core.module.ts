c018983d1bc27e2f67cc541dd3bf536bdeba9c00
irnq <47535562+irnq@users.noreply.github.com>
2022-04-27 21:39:25 +0400

    feat: add logoComponent

--- project-management-app/src/app/core/core.module.ts
+++ project-management-app/src/app/core/core.module.ts
@@ -3,9 +3,10 @@ import { SharedModule } from 'src/app/shared/shared.module';
 import { CoreRoutingModule } from './core-routing.module';
 import { HeaderComponent } from './header/header/header.component';
 import { NotFoundComponent } from './page/not-found/not-found.component';
+import { LogoComponent } from './header/logo/logo.component';
 
 @NgModule({
-  declarations: [HeaderComponent, NotFoundComponent],
+  declarations: [HeaderComponent, NotFoundComponent, LogoComponent],
   imports: [SharedModule, CoreRoutingModule],
   exports: [HeaderComponent],
 })
