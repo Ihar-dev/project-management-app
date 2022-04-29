46ff0bfb8e24ff85d06eac3c1664848d5b354e83
Tetiana Maslova <tetiana.maslova.qa@gmail.com>
2022-04-29 21:17:10 +0200

    refactor: update headers interceptor for delete method

--- project-management-app/src/app/interceptors/headers-request.interceptor.ts
+++ project-management-app/src/app/interceptors/headers-request.interceptor.ts
@@ -1,5 +1,11 @@
 import { Injectable } from '@angular/core';
-import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor,
+  HttpHeaders,
+} from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { LocalstorageService } from '../core/services/localstorage.service';
 import { USER_TOKEN_KEY } from '../auth/shared/constants';
@@ -10,15 +16,25 @@ export class HeadersRequestInterceptor implements HttpInterceptor {
   constructor(private lsStorage: LocalstorageService) {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    const authToken = this.lsStorage.getItem<string>(USER_TOKEN_KEY);
     const res = request.clone({
-      setHeaders: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-        Authorization: (authToken && `${TOKEN_TYPE} ${authToken}`) || '',
-      },
+      headers: this.getHeaders(request),
     });
 
     return next.handle(res);
+  }
+
+  private getHeaders(request: HttpRequest<unknown>): HttpHeaders {
+    const authToken = this.lsStorage.getItem<string>(USER_TOKEN_KEY);
+
+    let headers = new HttpHeaders()
+      .set('Accept', 'application/json')
+      .set('Authorization', (authToken && `${TOKEN_TYPE} ${authToken}`) || '');
+
+    // checking the request method. DELETE method should not be sent with a Content-Type header
+    if (request.method !== 'DELETE') {
+      headers = headers.set('Content-Type', 'application/json');
+    }
+
+    return headers;
   }
 }
