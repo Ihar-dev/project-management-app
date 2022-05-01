34011784813db110825b1aeb1699bb47b68cb9ff
Ihar Kozik <ihar.kozik@gmail.com>
2022-05-01 20:47:26 +0300

    feat: edit welcome component

--- project-management-app/src/app/main/pages/welcome/welcome.component.ts
+++ project-management-app/src/app/main/pages/welcome/welcome.component.ts
@@ -6,8 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./welcome.component.scss']
 })
 export class WelcomeComponent implements OnInit {
+  public pageActivation = false;
 
   ngOnInit(): void {
+    setTimeout(() => {
+      this.pageActivation = true;
+    });
   }
 
 }
