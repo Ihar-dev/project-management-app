5bbb9ba65e0bdb2989ab988fe12d6d09c3e25c46
irnq <47535562+irnq@users.noreply.github.com>
2022-05-01 11:37:32 +0400

    fix file structure

--- project-management-app/src/app/core/core.module.ts
+++ project-management-app/src/app/core/core.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { CoreRoutingModule } from './core-routing.module';
-import { HeaderComponent } from './header/header/header.component';
+import { HeaderComponent } from './components/header/header/header.component';
 import { NotFoundComponent } from './page/not-found/not-found.component';
-import { LogoComponent } from './header/logo/logo.component';
+import { LogoComponent } from './components/header/logo/logo.component';
 import { MaterialModule } from '../shared/material.module';
-import { UserBoxComponent } from './header/user-box/user-box.component';
-import { CreateButtonComponent } from './header/create-button/create-button.component';
+import { UserBoxComponent } from './components/header/user-box/user-box.component';
+import { CreateButtonComponent } from './components/header/create-button/create-button.component';
 
 @NgModule({
   declarations: [
