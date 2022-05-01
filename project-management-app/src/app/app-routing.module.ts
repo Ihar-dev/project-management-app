4bd1b935003a03baf12290621fdc9bf97591e3a6
Ihar Kozik <ihar.kozik@gmail.com>
2022-05-01 17:55:11 +0300

    feat: add main module and welcome component

--- project-management-app/src/app/app-routing.module.ts
+++ project-management-app/src/app/app-routing.module.ts
@@ -6,6 +6,11 @@ const routes: Routes = [
     path: 'auth',
     loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule),
   },
+  {
+    path: 'main',
+    loadChildren: () => import('./main/main.module').then((m) => m.MainModule),
+  },
+  { path: '', redirectTo: 'main/welcome', pathMatch: 'full' },
 ];
 
 @NgModule({
