8e80a5fbc980a3135807079ead520d9588b6d98d
Ihar Kozik <ihar.kozik@gmail.com>
2022-05-06 01:19:03 +0300

    feat: add main component

--- project-management-app/src/app/main/main-routing.module.ts
+++ project-management-app/src/app/main/main-routing.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { WelcomeComponent } from './pages/welcome/welcome.component';
+import { MainComponent } from './pages/main/main.component';
 
 const routes: Routes = [
   {
     path: 'welcome',
     component: WelcomeComponent,
+  },
+  {
+    path: '',
+    component: MainComponent,
   },
 ];
 
--- project-management-app/src/app/main/main.module.ts
+++ project-management-app/src/app/main/main.module.ts
@@ -1,18 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { CoreModule } from '../core/core.module';
+
 import { MainRoutingModule } from './main-routing.module';
 import { WelcomeComponent } from './pages/welcome/welcome.component';
 import { MaterialModule } from '../shared/material.module';
+import { MainComponent } from './pages/main/main.component';
+
 
 @NgModule({
   declarations: [
-    WelcomeComponent
+    WelcomeComponent,
+    MainComponent,
   ],
   imports: [
     CommonModule,
     MainRoutingModule,
     MaterialModule,
+    CoreModule
   ]
 })
 export class MainModule { }
