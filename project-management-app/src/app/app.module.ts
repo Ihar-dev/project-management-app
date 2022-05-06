01969b383c6866cb13e724595f65248a81eba565
irnq <----------+irnq@users.noreply.github.com>
2022-05-06 12:51:31 +0400

    feat: add ngrx-devtools

--- project-management-app/src/app/app.module.ts
+++ project-management-app/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { CoreModule } from './core/core.module';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -11,6 +12,7 @@ import { httpInterceptorProviders } from './interceptors';
 import * as boardReducer from './store/reducers/board.reducer';
 import { BoardEffects } from './store/effects/board.effect';
 import { ColumnEffects } from './store/effects/column.effect';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
@@ -22,6 +24,7 @@ import { ColumnEffects } from './store/effects/column.effect';
     CoreModule,
     StoreModule.forRoot({ board: boardReducer.reducer }, {}),
     EffectsModule.forRoot([BoardEffects, ColumnEffects]),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
   ],
   providers: [httpInterceptorProviders],
   bootstrap: [AppComponent],
