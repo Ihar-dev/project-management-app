843ecf4808ede1f5fa2848fe1654d394ae29d533
Ihar Kozik <ihar.kozik@gmail.com>
2022-05-08 23:47:04 +0300

    feat: create welcome module

--- project-management-app/src/app/welcome/welcome-routing.module.ts
+++ project-management-app/src/app/welcome/welcome-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { MainComponent } from './pages/main/main.component';
+import { WelcomeComponent } from './pages/welcome/welcome.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: MainComponent,
+    component: WelcomeComponent,
   },
 ];
 
@@ -14,4 +14,4 @@ const routes: Routes = [
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class MainRoutingModule { }
+export class WelcomeRoutingModule { }
