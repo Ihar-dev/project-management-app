2e329eecca10f31836d80c9878736f963bc4832b
irnq <47535562+irnq@users.noreply.github.com>
2022-05-11 23:53:17 +0400

    refactor

--- project-management-app/src/app/core/core.module.ts
+++ project-management-app/src/app/core/core.module.ts
@@ -3,6 +3,10 @@ import { SharedModule } from 'src/app/shared/shared.module';
 import { CoreRoutingModule } from './core-routing.module';
 
 import { NotFoundComponent } from './page/not-found/not-found.component';
+import { HeaderComponent } from './components/header/header/header.component';
+import { CreateButtonComponent } from './components/header/create-button/create-button.component';
+import { LogoComponent } from './components/header/logo/logo.component';
+import { UserBoxComponent } from './components/header/user-box/user-box.component';
 import { MessageBoxComponent } from './components/message-box/message-box.component';
 import { LoaderComponent } from './components/loader/loader.component';
 import { FooterComponent } from './components/footer/footer/footer.component';
@@ -12,13 +16,17 @@ import { DialogConfirmationComponent } from './components/dialog-confirmation/di
 @NgModule({
   declarations: [
     NotFoundComponent,
+    HeaderComponent,
     MessageBoxComponent,
     LoaderComponent,
     FooterComponent,
     AuthorsComponent,
     DialogConfirmationComponent,
+    CreateButtonComponent,
+    LogoComponent,
+    UserBoxComponent,
   ],
   imports: [SharedModule, CoreRoutingModule],
-  exports: [MessageBoxComponent, LoaderComponent, FooterComponent],
+  exports: [MessageBoxComponent, LoaderComponent, FooterComponent, HeaderComponent],
 })
 export class CoreModule {}
