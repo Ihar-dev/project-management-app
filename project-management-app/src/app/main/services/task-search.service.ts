fa20db300a544cd9ddc87c62f6daeebe79bc8582
Ihar Kozik <ihar.kozik@gmail.com>
2022-05-13 01:03:22 +0300

    refactor: edit task-search service

--- project-management-app/src/app/main/services/task-search.service.ts
+++ project-management-app/src/app/main/services/task-search.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
 
 import { IBoard } from '../../shared/models/board.model';
+import { IColumn } from '../../shared/models/column.model';
+import { ITask } from '../../shared/models/task.model';
 import { SearchResult } from '../models/search-result.model';
 
 @Injectable({
@@ -10,43 +12,44 @@ import { SearchResult } from '../models/search-result.model';
 export class TaskSearchService {
   public searchResults$ = new Subject < SearchResult[] >();
   public boards: IBoard[] = [];
+  private results: SearchResult[];
 
   public filter(boards: IBoard[], dataForSearch: string): void {
-    const results: SearchResult[] = [];
-    let result: SearchResult = {
-      board: {
-        title: '',
-        id: '',
-        description: '',
-        columns: [],
-      },
-      text: '',
-    };
+    this.results = [];
     const re = new RegExp(dataForSearch, 'i');
     boards.forEach(board => {
       if (board.columns) {
         board.columns.forEach(column => {
           if (column.tasks) {
             column.tasks.forEach(task => {
-              if (task.title.match(re)) {
-                result = {
-                  board,
-                  text: `Board: "${board.title}", column: "${column.title}", task title found: "${task.title}"`,
-                }
-                results.push(result);
-              }
-              if (task.description.match(re)) {
-                result = {
-                  board,
-                  text: `Board: "${board.title}", column: "${column.title}", task: "${task.title}", task description found: "${task.description}"`,
-                }
-                results.push(result);
-              }
+              this.titleSearch(board, column, task, re);
+              this.descriptionSearch(board, column, task, re);
             });
           }
         });
       }
     });
-    this.searchResults$.next(results);
+    this.searchResults$.next(this.results);
+  }
+
+  private titleSearch(board: IBoard, column: IColumn, task: ITask, re: RegExp): void {
+    if (task.title.match(re)) {
+      const result: SearchResult = {
+        board,
+        text: `Board: "${board.title}", column: "${column.title}", task title found: "${task.title}"`,
+      }
+      this.results.push(result);
+
+    }
+  }
+
+  private descriptionSearch(board: IBoard, column: IColumn, task: ITask, re: RegExp): void {
+    if (task.description.match(re)) {
+      const result: SearchResult = {
+        board,
+        text: `Board: "${board.title}", column: "${column.title}", task: "${task.title}", task description found: "${task.description}"`,
+      }
+      this.results.push(result);
+    }
   }
 }
