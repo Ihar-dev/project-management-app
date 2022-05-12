7ac90332c3ec405f9671ca53829ddb81688423c9
Ihar Kozik <ihar.kozik@gmail.com>
2022-05-12 20:10:55 +0300

    fix: get back ClickStopPropagationDirective

--- project-management-app/src/app/boards/boards.module.ts
+++ project-management-app/src/app/boards/boards.module.ts
@@ -8,6 +8,7 @@ import { BoardPageComponent } from './pages/board-page/board-page.component';
 import { BoardComponent } from './components/board/board.component';
 import { ColumnComponent } from './components/board/column/column.component';
 import { TaskComponent } from './components/board/column/task/task.component';
+import { ClickStopPropagationDirective } from './shared/directives/click-stop-propagation.directive';
 import { ButtonBoardDeleteComponent } from './shared/components/button-board-delete/button-board-delete.component';
 import { ButtonBoardCreateComponent } from './shared/components/button-board-create/button-board-create.component';
 import { TaskMemberComponent } from './components/board/column/task/task-member/task-member.component';
@@ -21,6 +22,7 @@ import { TaskMemberComponent } from './components/board/column/task/task-member/
     ButtonBoardDeleteComponent,
     ButtonBoardCreateComponent,
     TaskMemberComponent,
+    ClickStopPropagationDirective,
   ],
   imports: [CommonModule, BoardsRoutingModule, MaterialModule, SharedModule],
 })
