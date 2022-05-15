8ca39844ac744bfce3b0b874a54eb441f9816fdd
Ihar Kozik <ihar.kozik@gmail.com>
2022-05-16 00:49:57 +0300

    feat: edit BoardHandlingService

--- project-management-app/src/app/boards/components/board/column/column.component.ts
+++ project-management-app/src/app/boards/components/board/column/column.component.ts
@@ -1,14 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { Store } from '@ngrx/store';
 import { IColumn } from 'src/app/shared/models/column.model';
 
 import { MatDialog } from '@angular/material/dialog';
-import { DialogCreationComponent, DialogInterface } from 'src/app/shared/components/dialog-creation/dialog-creation.component';
-import { ColumnActions } from 'src/app/store/actions/column.action';
-import { DialogConfirmationComponent, DialogData }
-from '../../../../core/components/dialog-confirmation/dialog-confirmation.component';
-
-const DELETE_THE_BOARD_QUESTION = 'Are you sure you would like to delete the column?';
+import { BoardHandlingService } from '../../../../main/services/board-handling.service';
 
 @Component({
   selector: 'app-column',
@@ -18,14 +12,18 @@ const DELETE_THE_BOARD_QUESTION = 'Are you sure you would like to delete the col
 export class ColumnComponent implements OnInit {
   @Input() column: IColumn | null = null;
   @Input() boardID = '';
-  private columnID = '';
+  public columnID = '';
+  public columnTitle = '';
 
   editMode: boolean = false;
 
-  constructor(private readonly dialog: MatDialog, private readonly store: Store) {}
+  constructor(private readonly dialog: MatDialog, public readonly boardHandlingService: BoardHandlingService) {
+      this.boardHandlingService = boardHandlingService;
+    }
 
   ngOnInit(): void {
-    if (this.column?.id) this.columnID = this.column?.id;
+    if (this.column?.id) this.columnID = this.column.id;
+    if (this.column?.title) this.columnTitle = this.column.title;
   }
 
   onClickTitle(): void {
@@ -33,9 +31,7 @@ export class ColumnComponent implements OnInit {
   }
 
   onSubmitEditing(value: string): void {
-    if (this.column) {
-      this.column.title = value;
-    }
+    if (this.column) this.column.title = value;
     this.setEditMode(false);
   }
 
@@ -45,34 +41,5 @@ export class ColumnComponent implements OnInit {
 
   setEditMode(value: boolean): void {
     this.editMode = value;
-  }
-
-  openCreateTaskDialog(): void {
-    this.dialog.open(DialogCreationComponent,
-    { data: <DialogInterface> { type: 'task', columnID: this.columnID, boardID: this.boardID } });
-  }
-
-  public openDialogToDeleteTheColumn(): void {
-    const dialogRef = this.dialog.open(DialogConfirmationComponent, {
-      data: <DialogData>{
-        h2: DELETE_THE_BOARD_QUESTION,
-        p: `${this.column?.title}`,
-      },
-    });
-
-    dialogRef.afterClosed().subscribe((result: Response) => {
-      if (result) {
-        this.deleteColumn(this.boardID, this.columnID);
-      }
-    });
-  }
-
-  deleteColumn(boardID: string, columnID: string): void {
-    this.store.dispatch(
-      ColumnActions.deleteColumn({
-        boardID,
-        columnID,
-      }),
-    );
   }
 }
--- project-management-app/src/app/main/services/board-handling.service.ts
+++ project-management-app/src/app/main/services/board-handling.service.ts
@@ -1,10 +1,19 @@
 import { Injectable } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Observable, Subject } from 'rxjs';
+import { MatDialog } from '@angular/material/dialog';
 
+import { ColumnActions } from 'src/app/store/actions/column.action';
 import { BoardActions } from 'src/app/store/actions/board.action';
+import { DialogCreationComponent, DialogInterface } from 'src/app/shared/components/dialog-creation/dialog-creation.component';
+import { DialogConfirmationComponent, DialogData }
+from '../../core/components/dialog-confirmation/dialog-confirmation.component';
 import { IBoard } from '../../shared/models/board.model';
 import { BoardSelectors } from '../../store/selectors/board.selector';
+
+enum DeleteQuestions {
+  column = 'Are you sure you would like to delete the column?',
+}
 
 @Injectable({
   providedIn: 'root'
@@ -20,7 +29,7 @@ export class BoardHandlingService {
     columns: [],
   };
 
-  constructor(private readonly store: Store) {
+  constructor(private readonly store: Store, private readonly dialog: MatDialog) {
     this.getBoards();
     this.boards$ = this.store.select(BoardSelectors.selectBoards);
     this.boards$.subscribe((boards: IBoard[]) => {
@@ -44,5 +53,47 @@ export class BoardHandlingService {
 
   private getBoards(): void {
     this.store.dispatch(BoardActions.getBoards());
+  }
+
+  public openDialogToDelete(entity: string, title: string, boardID: string, columnID: string): void {
+    let question: string;
+    switch (entity) {
+      case 'column':
+        question = DeleteQuestions.column;
+        break;
+      default:
+        question = '';
+    }
+    const dialogRef = this.dialog.open(DialogConfirmationComponent, {
+      data: <DialogData>{
+        h2: question,
+        p: title,
+      },
+    });
+
+    dialogRef.afterClosed().subscribe((result: Response) => {
+      if (result) {
+        switch (entity) {
+          case 'column':
+            this.deleteColumn(boardID, columnID);
+            break;
+          default:
+        }
+      }
+    });
+  }
+
+  deleteColumn(boardID: string, columnID: string): void {
+    this.store.dispatch(
+      ColumnActions.deleteColumn({
+        boardID,
+        columnID,
+      }),
+    );
+  }
+
+  openCreateTaskDialog(boardID: string, columnID: string): void {
+    this.dialog.open(DialogCreationComponent,
+    { data: <DialogInterface> { type: 'task', boardID, columnID } });
   }
 }
