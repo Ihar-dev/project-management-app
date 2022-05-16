ef5f5f56232253623c2e5cf6ce75a5e56f7936ea
Tetiana Maslova <tetiana.maslova.qa@gmail.com>
2022-05-16 14:37:29 +0200

    refactor: add client side error handling

--- project-management-app/src/app/interceptors/error-handler.interceptor.ts
+++ project-management-app/src/app/interceptors/error-handler.interceptor.ts
@@ -1,12 +1,6 @@
 import { Injectable } from '@angular/core';
-import {
-  HttpRequest,
-  HttpHandler,
-  HttpEvent,
-  HttpInterceptor,
-  HttpErrorResponse,
-} from '@angular/common/http';
-import { catchError, Observable } from 'rxjs';
+import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
+import { catchError, EMPTY, Observable } from 'rxjs';
 import { HttpErrorService } from '../core/services/http-error.service';
 
 @Injectable()
@@ -16,16 +10,17 @@ export class ErrorHandlerInterceptor implements HttpInterceptor {
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     return next.handle(request).pipe(
       catchError((error) => {
-        if (error instanceof HttpErrorResponse) {
+        if (error instanceof ErrorEvent) {
           this.logMessage(error);
+          return EMPTY;
         }
+
         return this.httpErrorService.handleError(error);
       }),
     );
   }
 
-  private logMessage(error: HttpErrorResponse): void {
-    const errorMessage = Array.isArray(error.message) ? error.message.join(',') : error.message;
-    console.log(errorMessage);
+  private logMessage(error: ErrorEvent): void {
+    console.log(error.message);
   }
 }
