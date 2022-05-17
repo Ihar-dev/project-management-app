43c483233f2dba15b3f4b4ba027dbfca7a9c9417
Tetiana Maslova <tetiana.maslova.qa@gmail.com>
2022-05-17 16:11:20 +0200

    feat: add error messages for columns

--- project-management-app/src/app/store/effects/column.effect.ts
+++ project-management-app/src/app/store/effects/column.effect.ts
@@ -1,25 +1,30 @@
 import { Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { catchError, map, mergeMap } from 'rxjs';
+import { ErrorMessages } from 'src/app/shared/models/error-messages.model';
+import { IHttpErrorMessage } from 'src/app/shared/models/http-error-message.model';
 import { ColumnDbService } from 'src/app/shared/services/column-db.service';
-import { EffectsHandlerService } from 'src/app/shared/services/effects-handler.service';
-import { ColumnActions } from '../actions/column.action';
-
-enum Operation {
-  AddColumn = 'Add column',
-  DeleteColumn = 'Delete column',
-  PutColumn = 'Put column',
-}
+import { ColumnAction, ColumnActions } from '../actions/column.action';
 
 @Injectable()
 export class ColumnEffects {
+  errorMessages: IHttpErrorMessage[] = ErrorMessages.columnMessages;
+
   addColumn$ = createEffect(() =>
     this.actions$.pipe(
       ofType(ColumnActions.addColumn),
       mergeMap((action) =>
         this.dbService.addColumn(action.boardID, action.column).pipe(
           map((column) => ColumnActions.addColumnSuccess({ boardID: action.boardID, column })),
-          catchError(this.handler.handleError(Operation.AddColumn)),
+          catchError((err) => [
+            ColumnActions.columnError({
+              data: {
+                error: err,
+                actionType: ColumnAction.AddColumn,
+                messages: this.errorMessages,
+              },
+            }),
+          ]),
         ),
       ),
     ),
@@ -31,7 +36,15 @@ export class ColumnEffects {
       mergeMap((action) =>
         this.dbService.deleteColumn(action.boardID, action.columnID).pipe(
           map(() => ColumnActions.deleteColumnSuccess({ ...action })),
-          catchError(this.handler.handleError(Operation.DeleteColumn)),
+          catchError((err) => [
+            ColumnActions.columnError({
+              data: {
+                error: err,
+                actionType: ColumnAction.DeleteColumn,
+                messages: this.errorMessages,
+              },
+            }),
+          ]),
         ),
       ),
     ),
@@ -43,15 +56,19 @@ export class ColumnEffects {
       mergeMap((action) =>
         this.dbService.updateColumn(action.boardID, { ...action.column }).pipe(
           map((column) => ColumnActions.putColumnSuccess({ boardID: action.boardID, column })),
-          catchError(this.handler.handleError(Operation.DeleteColumn)),
+          catchError((err) => [
+            ColumnActions.columnError({
+              data: {
+                error: err,
+                actionType: ColumnAction.PutColumn,
+                messages: this.errorMessages,
+              },
+            }),
+          ]),
         ),
       ),
     ),
   );
 
-  constructor(
-    private actions$: Actions,
-    private dbService: ColumnDbService,
-    private handler: EffectsHandlerService,
-  ) {}
+  constructor(private actions$: Actions, private dbService: ColumnDbService) {}
 }
