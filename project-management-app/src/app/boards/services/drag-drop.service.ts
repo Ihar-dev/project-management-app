2f7fe448dce733eeb9c4ed7cce23415ee679065f
Ihar Kozik <ihar.kozik@gmail.com>
2022-05-17 23:38:32 +0300

    feat: add task moving

--- project-management-app/src/app/boards/services/drag-drop.service.ts
+++ project-management-app/src/app/boards/services/drag-drop.service.ts
@@ -1,71 +1,20 @@
-import { Observable } from 'rxjs';
-import {
-  Injectable
-} from '@angular/core';
-import {
-  Store
-} from '@ngrx/store';
-import {
-  CdkDragDrop,
-  moveItemInArray,
-  transferArrayItem
-} from '@angular/cdk/drag-drop';
+import { Injectable } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 
-import {
-  TaskActions
-} from 'src/app/store/actions/task.action';
-import {
-  ColumnActions
-} from 'src/app/store/actions/column.action';
-import {
-  ITask
-} from 'src/app/shared/models/task.model';
-import {
-  IColumn
-} from 'src/app/shared/models/column.model';
-import {
-  IBoard
-} from 'src/app/shared/models/board.model';
-import {
-  ITaskRequest
-} from 'src/app/shared/models/task-request.model';
-
-import { BoardSelectors } from '../../store/selectors/board.selector';
+import { TaskActions } from 'src/app/store/actions/task.action';
+import { ColumnActions } from 'src/app/store/actions/column.action';
+import { ITask } from 'src/app/shared/models/task.model';
+import { IColumn } from 'src/app/shared/models/column.model';
+import { IBoard } from 'src/app/shared/models/board.model';
+import { ITaskRequest } from 'src/app/shared/models/task-request.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DragDropService {
-  private boards$: Observable < IBoard[] >;
-  private id = '';
-  private columnID = '';
-  private newColumn: ITask[] = [];
 
-  constructor(private readonly store: Store) {
-    this.boards$ = this.store.select(BoardSelectors.selectBoards);
-    this.boards$.subscribe((boards: IBoard[]) => {
-      boards.forEach(board => {
-        if (board.id === this.id) {
-          board.columns.forEach((column: IColumn) => {
-            if (column.id === this.columnID) {
-              console.log(column);
-              console.log(this.newColumn);
-
-/*               this.newColumn.forEach((task: ITask) => {
-
-                this.updateTask(this.id, this.columnID, task);
-
-
-
-              }); */
-
-
-            }
-          });
-        }
-      });
-    });
-  }
+  constructor(private readonly store: Store) {}
 
   public moveTask(event: CdkDragDrop < ITask[] > , boardID: string, column: IColumn): void {
     const columnID = column.id;
@@ -84,9 +33,7 @@ export class DragDropService {
   }
 
   public moveTaskDifferentColumn(event: CdkDragDrop < ITask[] > , boardID: string, column: IColumn, columns: IColumn[]): void {
-    this.id = boardID;
     const columnID = column.id;
-    this.columnID = columnID;
     const newTargetColumn = JSON.parse(JSON.stringify(event.container.data));
     const newPreviousColumn = JSON.parse(JSON.stringify(event.previousContainer.data));
     transferArrayItem(
@@ -101,11 +48,27 @@ export class DragDropService {
       return task;
     });
     this.addTask(boardID, columnID, prevTask);
-    this.newColumn = newTargetColumn;
     newTargetColumn.forEach((task: ITask, index: number) => {
       if (event.currentIndex < index) this.updateTask(boardID, columnID, task);
     });
+    const taskToDelete: ITask = event.previousContainer.data[event.previousIndex];
+    console.log(taskToDelete);
     console.log(columns);
+    columns.forEach((prevColumn: IColumn) => {
+      prevColumn.tasks.forEach((task: ITask) => {
+        if (task.id === taskToDelete.id) this.deleteTask(boardID, prevColumn.id, taskToDelete.id);
+      });
+    });
+  }
+
+  private deleteTask(boardID: string, columnID: string, taskID: string): void {
+    this.store.dispatch(
+      TaskActions.DeleteTask({
+        boardID,
+        columnID,
+        taskID,
+      }),
+    );
   }
 
   private updateTask(
