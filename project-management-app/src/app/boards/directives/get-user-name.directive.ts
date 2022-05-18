a9a90a42cdab9e688c2cb79b8c8fc54258fdb306
Ihar Kozik <ihar.kozik@gmail.com>
2022-05-18 22:11:47 +0300

    fix: edit GetUserNameDirective

--- project-management-app/src/app/boards/directives/get-user-name.directive.ts
+++ project-management-app/src/app/boards/directives/get-user-name.directive.ts
@@ -1,11 +1,11 @@
-import { Directive, ElementRef, Input, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, Input, OnInit, Renderer2 } from '@angular/core';
 
 import { ITask } from 'src/app/shared/models/task.model';
 
 @Directive({
-  selector: '[appGetUserName]'
+  selector: '[appGetUserName]',
 })
-export class GetUserNameDirective {
+export class GetUserNameDirective implements OnInit {
   @Input() public appGetUserName: ITask | null;
 
   constructor(private el: ElementRef, private renderer: Renderer2) {
@@ -15,5 +15,4 @@ export class GetUserNameDirective {
   ngOnInit(): void {
     this.renderer.setValue(this.el.nativeElement, 'uuu');
   }
-
 }
