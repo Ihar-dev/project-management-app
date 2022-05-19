a459be6e8834d2f6ba91d80ea2e5ba4ce6153109
irnq <47535562+irnq@users.noreply.github.com>
2022-05-19 22:40:04 +0400

    remove interceptor and log

--- project-management-app/src/app/interceptors/index.ts
+++ project-management-app/src/app/interceptors/index.ts
@@ -2,11 +2,9 @@ import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { BaseApiInterceptor } from './base-api.interceptor';
 import { LoaderInterceptor } from './loader.interceptor';
 import { HeadersRequestInterceptor } from './headers-request.interceptor';
-import { ErrorHandlerInterceptor } from './error-handler.interceptor';
 
 export const httpInterceptorProviders = [
   { provide: HTTP_INTERCEPTORS, useClass: LoaderInterceptor, multi: true },
   { provide: HTTP_INTERCEPTORS, useClass: BaseApiInterceptor, multi: true },
   { provide: HTTP_INTERCEPTORS, useClass: HeadersRequestInterceptor, multi: true },
-  { provide: HTTP_INTERCEPTORS, useClass: ErrorHandlerInterceptor, multi: true },
 ];
